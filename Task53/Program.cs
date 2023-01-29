137886c98a927ed9762902a7a306d0d5197c2d1a
Anastasiya Garkavenko <119501129+NaisiG@users.noreply.github.com>
2023-01-30 03:01:26 +0800

    Task

---
+++ Task53/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // столбец
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],  5}, ");
+            else Console.Write($"{matrix[i, j], 5}  ");            
+        }
+        Console.WriteLine("]");
+        
+    }
+}
+
+void ChangeFirsAndLastRows(int[,] matrixForChange)
+{
+    int firstRow = 0;
+    int lastRow = matrixForChange.GetLength(0) - 1;
+    for (int i = 0; i < matrixForChange.GetLength(0); i++) 
+    {
+        int temp = matrixForChange[firstRow, i];
+        matrixForChange[firstRow, i] = matrixForChange[lastRow, i];
+        matrixForChange[lastRow, i] = temp;
+    }    
+}
+
+
+int matrixRows = 5;
+int matrixColumns = 5;
+int matrixMinVolue = 1;
+int matrixMaxVolue =11;
+
+int[,] matrix = CreateMatrixRndInt(matrixRows, matrixColumns, matrixMinVolue, matrixMaxVolue);
+PrintMatrix(matrix);
+ChangeFirsAndLastRows(matrix);
+Console.WriteLine(string.Empty);
+PrintMatrix(matrix);
---
+++ Task55/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // столбец
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+
+// int[,] ChangeRowColunsMatrix(int[,] matrix)
+// // {
+//     int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];  //один из вариантов
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             newMatrix[i, j] = matrix[j, i];
+//         }
+//     }
+//     return newMatrix;
+// }
+void ChangeRowColunsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+
+int matrixRows = 4;
+int matrixColumns = 4;
+int matrixMinVolue = 1;
+int matrixMaxVolue = 11;
+
+int[,] matrixRnd = CreateMatrixRndInt(matrixRows, matrixColumns, matrixMinVolue, matrixMaxVolue);
+PrintMatrix(matrixRnd);
+
+if (matrixRows == matrixColumns)
+{
+    Console.WriteLine(string.Empty);
+    ChangeRowColunsMatrix(matrixRnd);
+    // int[,] newMatrix = ChangeRowColunsMatrix(matrixRnd);
+    //PrintMatrix(newMatrix);
+    PrintMatrix(matrixRnd);
+}
+else Console.WriteLine("Количество столбцов не равно количеству строк");
+
