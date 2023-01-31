3139666d83d13e22e973d007ef468d8d9f238041
Anastasiya Garkavenko <119501129+NaisiG@users.noreply.github.com>
2023-01-31 16:28:31 +0800

    Task

---
+++ Task57/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+// 25 мин
+// Набор данных Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 } 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // столбец
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} ");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+
+int[] NumberDictionary(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++) // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // столбец
+        {
+            array[index] = matrix[i, j];
+            index++;
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine();
+}
+
+void Dictionary(int[] array)
+{
+    int number = array[0];
+    int count = 1;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i] == number) count++;
+        else
+        {
+            Console.WriteLine($"{number} -> {count}");
+            number = array[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"{number} -> {count}");
+}
+
+int matrixRows = 5;
+int matrixColumns = 5;
+int matrixMinVolue = 1;
+int matrixMaxVolue = 10;
+int randLenght = matrixMaxVolue - matrixMinVolue - 1;
+
+int[,] firstMatrix = CreateMatrixRndInt(matrixRows, matrixColumns, matrixMinVolue, matrixMaxVolue);
+PrintMatrix(firstMatrix);
+Console.WriteLine(String.Empty);
+
+int[] firstArray = NumberDictionary(firstMatrix);
+PrintArray(firstArray);
+Console.WriteLine(String.Empty);
+Array.Sort(firstArray); //сортировка массива
+PrintArray(firstArray);
+Console.WriteLine(String.Empty);
+Dictionary(firstArray);
