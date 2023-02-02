28af0c6c05b9638548aaf8991a6a771ee03d0d78
Anastasiya Garkavenko <119501129+NaisiG@users.noreply.github.com>
2023-02-02 09:01:41 +0800

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,55 @@
+﻿int n = 5;
+int[,] matrix3 = new int[n, n];
+int num = 1;
+int delta = 0;
+int beta = matrix3.GetLength(0) - 1;
+int var = matrix3.GetLength(0) - 1;
+
+while(beta >= delta )
+{
+    for(int i = 0 + delta; i < beta; i++)
+    {
+        matrix3[0 + delta,i] = num;
+        num++;
+    }
+    
+    for(int i = 0 + delta; i < beta; i++)
+    {
+        matrix3[i,var - delta] = num;
+        num++;
+    }
+    
+    for(int i = 0 + beta; i >= delta; i--)
+    {
+        matrix3[var - delta,i] = num;
+        num++;
+    }
+    
+    num--;
+    for(int i = 0 + beta; i >= delta + 1; i--)
+    {
+        matrix3[i,0 + delta] = num;
+        num++;
+    }
+
+    delta++;
+    beta--;
+    
+    
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} ");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+PrintMatrix(matrix3);
